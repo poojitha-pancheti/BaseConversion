af5823a4b9abdf64831df12e5fbbfb5d0adeeea6
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 16:43:58 +0530

    Add project files.

---
+++ BaseConversion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BaseConversion
+{
+    class Program
+    {
+        public static void ToBinary(int n)
+        {
+            if (n == 0)
+                return;
+
+            ToBinary(n / 2);
+            Console.WriteLine(n % 2);
+        }
+        public static void ConvertBase(int n,int b)
+        {
+            if (n == 0)
+                return;
+            ConvertBase(n / b, b);
+            int remainder = n % 10;
+            if (remainder < 10)
+                Console.Write(remainder);
+            else
+                Console.Write((char)(remainder - 10 + 'A'));
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a positive decimal number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Binary form:"); ToBinary(n);
+            Console.WriteLine();
+            Console.Write("Binary form:"); ConvertBase(n, 2);
+            Console.WriteLine();
+            Console.Write("Octal form:"); ConvertBase(n, 8);
+            Console.WriteLine();
+            Console.Write("Hexadecimal form:"); ConvertBase(n, 16);
+            Console.WriteLine();
+        }
+    }
+}
